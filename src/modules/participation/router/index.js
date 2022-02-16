a2dff4ec2a86bec529a356ffa5cb38b27d7df898
Alexander Agrazal <a3rxander@gmail.com>
2022-02-15 22:48:37 -0500

    Users composition API - composables

--- src/modules/participation/router/index.js
+++ src/modules/participation/router/index.js
@@ -3,26 +3,31 @@ export default{
     name: 'participation',
     component: () => import(/* webpackChunkName: "participation" */ '@/modules/participation/layouts/ParticipationLayout.vue'),
     children: [
-    {
-        path:'',
-        name:'participation_index',
-        component: () => import(/* webpackChunkName: "participation_index"*/'@/modules/participation/views/PropuestaIndex.vue' )
-    },
-    {
-        path:'propuestas/create',
-        name:'participation_propuesta_create',
-        component: () => import(/* webpackChunkName: "participation_propuesta_create"*/'@/modules/participation/views/PropuestaCreate.vue' )
-    },
-    {
-        path:'propuestas/:id',
-        name:'participation_propuesta_show',
-        component: () => import(/* webpackChunkName: "participation_propuesta_show"*/'@/modules/participation/views/PropuestaShow.vue' ),
-        props:( route ) => {
-            return {
-                id:route.params.id
+        {
+            path:'',
+            name:'participation_index',
+            component: () => import(/* webpackChunkName: "participation_index"*/'@/modules/participation/views/PropuestaIndex.vue' )
+        },
+        {
+            path:'usuarios',
+            name:'participation_usuarios',
+            component: () => import(/* webpackChunkName: "participation_usuarios"*/'@/modules/participation/views/Usuarios.vue' )
+        },
+        {
+            path:'propuestas/create',
+            name:'participation_propuesta_create',
+            component: () => import(/* webpackChunkName: "participation_propuesta_create"*/'@/modules/participation/views/PropuestaCreate.vue' )
+        },
+        {
+            path:'propuestas/:id',
+            name:'participation_propuesta_show',
+            component: () => import(/* webpackChunkName: "participation_propuesta_show"*/'@/modules/participation/views/PropuestaShow.vue' ),
+            props:( route ) => {
+                return {
+                    id:route.params.id
+                }
             }
-        }
-    },
+        },
 
 ]
 }
